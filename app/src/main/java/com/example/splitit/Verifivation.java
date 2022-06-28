596eaf4bb08dbdf222a15d86ae757c2c0689dabf
sachin8340 <sachin900555@gmail.com>
2022-06-28 13:49:57 +0530

    verificatin activity

---
+++ app/src/main/java/com/example/splitit/Verifivation.java
@@ -0,0 +1,14 @@
+package com.example.splitit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Verifivation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verifivation);
+    }
+}
