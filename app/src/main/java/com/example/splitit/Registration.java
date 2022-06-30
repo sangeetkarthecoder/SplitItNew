67786218210297ede54170cdec43a5812a3a0532
iamryuga <kumarshi605@gmail.com>
2022-06-30 12:53:17 +0530

    verify

--- app/src/main/java/com/example/splitit/Registration.java
+++ app/src/main/java/com/example/splitit/Registration.java
@@ -19,7 +19,7 @@ public class Registration extends AppCompatActivity {
         linear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               
+
             }
         });
     }
