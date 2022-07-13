471f17d0f840d31bc26c454c440f819ca7aa79c1
iamryuga <78287034+iamryuga@users.noreply.github.com>
2022-07-13 05:49:59 -0700

    Revert "NEW UI"

--- app/src/main/java/com/example/splitit/VerifyPhone.java
+++ app/src/main/java/com/example/splitit/VerifyPhone.java
@@ -1,26 +1,14 @@
 package com.example.splitit;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.splitit.databinding.ActivityPhoneAuthBinding;
-import com.example.splitit.databinding.ActivityVerifyPhoneBinding;
-
 public class VerifyPhone extends AppCompatActivity {
-
-    ActivityVerifyPhoneBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityVerifyPhoneBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        binding.otpv.requestFocus();
-        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.showSoftInput(binding.otpv, InputMethodManager.SHOW_IMPLICIT);
+        setContentView(R.layout.activity_verify_phone);
     }
 }
